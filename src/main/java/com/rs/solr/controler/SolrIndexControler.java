4dc918855a3d6abbd576d04a87b608458053b826
rudrasagar.tn <rudrasagar.tn@VDC01HLAP0484.IN.valtech.com>
2020-06-02 20:51:37 +0530

    Added RequestParameter & MethodType as Get & Post

--- src/main/java/com/rs/solr/controler/SolrIndexControler.java
+++ src/main/java/com/rs/solr/controler/SolrIndexControler.java
@@ -10,7 +10,10 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -22,29 +25,31 @@ public class SolrIndexControler {
 
 	@Autowired
 	private SolrProductRepository solrProductRepository;
-	
+
 	@Autowired
 	private RestTemplate restTemplate;
 
-	@RequestMapping(value = "/findById")
-	public Optional<Product> findByProductId() {
-		return solrProductRepository.findById(new Long(884355));
+	@RequestMapping(value = "/findById", method = RequestMethod.GET)
+	public Optional<Product> findByProductId(@RequestParam Optional<Long> productId) {
+		return solrProductRepository.findById(productId.get());
 	}
 
-	@RequestMapping(value = "/findByName")
-	public List<Product> findByName() {
-		return solrProductRepository.findByName2("18188");
+	@RequestMapping(value = "/findByName", method = RequestMethod.GET)
+	public List<Product> findByName(@RequestParam(name = "productName") String productName) {
+		return solrProductRepository.findByName2(productName);
 	}
-	
-	@RequestMapping(value = "/deltaImport")
-	public void deltaImport() {
+
+	@RequestMapping(value = "/deltaImport", method = RequestMethod.POST)
+	public void deltaImport(@RequestHeader HttpHeaders requestHeader) {
 		HttpHeaders headers = new HttpHeaders();
-	      headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-	      HttpEntity <String> entity = new HttpEntity<String>(headers);
-	      ResponseEntity<String> e = 
-		restTemplate.exchange(
-		         "http://localhost:8983/solr/item_product/dataimport?command=delta-import&commit=true&clean=false&debug=true", HttpMethod.GET, entity, String.class);
-	     
-	      System.out.println(e);
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		HttpEntity<String> entity = new HttpEntity<String>(headers);
+		ResponseEntity<String> e = null;
+		if (requestHeader.get("password").isEmpty()) {
+			e = restTemplate.exchange(
+					"http://localhost:8983/solr/item_product/dataimport?command=delta-import&commit=true&clean=false&debug=true",
+					HttpMethod.GET, entity, String.class);
+		}
+		System.out.println(requestHeader.get("password") + " Response is : " + e);
 	}
 }
